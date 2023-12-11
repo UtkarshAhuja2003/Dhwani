263b2b6ae9961f78def38b90ab2bbd6bb94126a8
kaif-stack <mdkaif2938@gmail.com>
2023-12-12 01:05:33 +0530

    bottom navbar

--- lib/views/dhwani_main/dhwani/dhwani_detail_screen.dart
+++ lib/views/dhwani_main/dhwani/dhwani_detail_screen.dart
@@ -1,4 +1,3 @@
-import 'package:dhwani/views/dhwani_main/dhwani/image_view.dart';
 import 'package:flutter/material.dart';
 
 class DhwaniDetailScreen extends StatelessWidget {
@@ -6,18 +5,138 @@ class DhwaniDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Material(
-      child: ListView(
-        children: [
-          const ImageView(),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              'Your Scrollable Text Here', // Example long text
-              style: TextStyle(fontSize: 18.0),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: screenWidth,
+              height: 150,
+              decoration: const BoxDecoration(
+                color: Color(0xffE6F4F1),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            elevation: 5,
+                            backgroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: const Text(
+                            "अगला खंड",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 23,
+                              fontFamily: "NotoSansDevanagari",
+                              color: Color(0xff04434E),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "ओष्ठय ध्वनियाँ",
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            Container(
+              color: const Color(0xff04434E),
+              width: screenWidth,
+              height: 2,
+            ),
+             Padding(
+               padding: const EdgeInsets.only(left: 30, right: 30),
+               child: Image(
+                image: const AssetImage("assets/speech-mechanism-new.png"),
+                width: screenWidth,
+                fit: BoxFit.fill,
+                           ),
+             ),
+            const Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Text("ओष्ठय-ध्वनियाँ वे होती हैं, जो हमारे होंठों को मिलाकर निकलती हैं।",
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  fontSize: 23,
+                  fontFamily: "NotoSansDevanagari",
+                  color: Color(0xff04434E),
+                ),),
+            ),
+            const Text("हिंदी में 5 मुख्य ओष्ठय ध्वनियाँ हैं :",
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  fontSize: 23,
+                  fontFamily: "NotoSansDevanagari",
+                  color: Color(0xff04434E),
+                ),),
+            GridView.builder(
+              physics: const ScrollPhysics(),
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(32.0),
+              gridDelegate:const  SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 5,
+                crossAxisSpacing: 25.0,
+                mainAxisSpacing: 25.0,
+              ),
+              itemCount: 5,
+              itemBuilder: (BuildContext context, int index) {
+                return const Text("प",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 50,
+                    fontFamily: "NotoSansDevanagari",
+                    color: Color(0xff04434E),
+                  ),);
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 30, top: 10, bottom: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      elevation: 10,
+                      backgroundColor: const Color(0xff0CC0DF),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 10, right: 10),
+                      child: const Icon(Icons.arrow_forward, color: Colors.white,),
+                    )
+                  ),
+                ],
+              ),
+            ),
+
+
+          ],
+        ),
       ),
     );
   }
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,14 +1,55 @@
 // import 'package:dhwani/views/auth/signup_view.dart';
+import 'package:dhwani/views/dhwani_main/dhwani/dhwani_detail_screen.dart';
+import 'package:dhwani/views/dhwani_main/dhwani/dhwani_example/dhwani_example_screen.dart';
 import 'package:dhwani/views/dhwani_main/dhwani_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
-  const HomeView({super.key});
+  const HomeView({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(bottom:false,child: DhwaniScreen()),
+    return Scaffold(
+      body: const SafeArea(bottom: false, child: DhwaniDetailScreen()),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(40),
+          topRight: Radius.circular(40),
+        ),
+        child: BottomAppBar(
+          color: const Color(0xffE6FAFD),
+          elevation: 0, // Optional: Set elevation to 0 to remove shadow
+          child: Container(
+            height: 67,
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(
+                  Icons.home,
+                  size: 45,
+                  color: Color(0xff0CC0DF),
+                ),
+                Icon(
+                  Icons.home,
+                  size: 45,
+                  color: Color(0xff0CC0DF),
+                ),
+                Icon(
+                  Icons.home,
+                  size: 45,
+                  color: Color(0xff0CC0DF),
+                ),
+                Icon(
+                  Icons.home,
+                  size: 45,
+                  color: Color(0xff0CC0DF),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
+
